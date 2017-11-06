660b9f435c329836bcb205f42d7b77a14dc9def3
Erik Kimsey <erik@erikkimsey.com>
2017-11-06 15:18:18 -0500

    Removed some redundant code in List.vue

--- src/components/List.vue
+++ src/components/List.vue
@@ -29,11 +29,6 @@ export default {
     fetchList(){
       this.$http.get('http://hn.algolia.com/api/v1/search?query=javascript&hitsPerPage=25').then((response) => {
          this.items = response.data.hits;
-         var i;
-         for(i=0; i<25; i++){
-           this.items[i].push = this.items[i];
-           console.log(this.items);
-         }
        })
     }
   }
